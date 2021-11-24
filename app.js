36e813170779f92dff9eb16616a6db41c18dc17a
Yeseul Lee <dev.yezlee@gmail.com>
2021-11-24 15:15:12 +0000

    Fetch data with JSON - "It's Kanye time"

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("It's Kanye time...");
+
+async function getQuote() {
+  const response = await fetch("https://api.kanye.rest");
+  const data = await response.json();
+  console.log(data);
+
+  const h1 = document.querySelector("#kanye-quote");
+  h1.innerText = data.quote;
+}
+
+const button = document.querySelector("#new-quote-button");
+button.addEventListener("click", getQuote);
+
+// getQuote();
